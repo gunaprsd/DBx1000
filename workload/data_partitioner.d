8cde158ad3c31bc6140a894fffcf8d9388e3e1e5
Guna Prasaad <gunaprsd@gmail.com>
2017-12-12 18:54:02 -0800

    deleting .d files

--- workload/data_partitioner.d
+++
@@ -1,4 +0,0 @@
-workload/data_partitioner.o: workload/data_partitioner.cpp \
- workload/data_partitioner.h workload/graph_partitioner.h system/global.h \
- config.h system/stats.h cc/dl_detect.h system/helper.h \
- lib/include/metis.h system/query.h
--- workload/partitioner.d
+++
@@ -1,3 +0,0 @@
-workload/partitioner.o: workload/partitioner.cpp workload/partitioner.h \
- workload/graph_partitioner.h system/global.h config.h system/stats.h \
- cc/dl_detect.h system/helper.h lib/include/metis.h system/query.h

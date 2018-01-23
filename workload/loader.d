f67bd0665cd149baf3e31ead428d668054d1a4a5
Guna Prasaad <gunaprsd@gmail.com>
2018-01-22 23:45:19 -0800

    minor deletes

--- workload/loader.d
+++
@@ -1,3 +0,0 @@
-workload/loader.o: workload/loader.cpp workload/loader.h system/query.h \
- system/global.h lib/include/metis.h system/config.h cc/dl_detect.h \
- config.h system/stats.h system/helper.h
